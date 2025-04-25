4f79195533960ac1e8a78f25832b52e582f2cc78
bae <bottleright10@gmail.com>
2025-04-25 16:11:05 +0900

    2025-04-25

--- PythonBasicProject/python_basic/basic_7.py
+++ PythonBasicProject/python_basic/basic_7.py
@@ -8,3 +8,45 @@
    => msg[0] => 추출(배열형식으로 추출)
    => 슬라이싱
 '''
+msg="Hello Python"
+print(msg)
+print(msg[0])
+for s in msg:
+    print(s,end="")
+print()
+ss="ABCDEFGHIJ"
+#   01234...
+#           -2-1
+print(f"ss={ss}")
+print(f"ss[0]={ss[0]}") # 처음 문자
+print(f"ss[-1]={ss[-1]}")# 마지막 문자
+print(f"ss[0:3]={ss[0:3]}")# 처음부터 3글자
+print(f"ss[1:]={ss[1:]}")#두번째부터 마지막 문자열 까지
+print(f"ss[-3:-1]={ss[-3:-1]}")
+print(f"ss[:]={ss[:]}")# 전체 출력
+print(f"ss[0:3]={ss[0:3:2]}") #012
+'''
+    문자열의 주요 함수
+    
+'''
+data="Hello Python"
+#     01234567891011
+print(data.upper()) # 대문자
+print(data.lower()) # 소문자
+# trim() : 공백제거
+print(data.rstrip()) # 오른쪽 공백 제거
+print(data.lstrip()) # 왼쪽 공백 제거
+print(data.strip()) # 오른쪽,왼쪽 제거
+# 대문자 => 소문자 , 소문자 => 대문자
+print(data.swapcase())
+# 문자 길이
+print(data.__len__())
+print(len(data))
+#시작 문자열
+print(data.startswith("He"))
+#첫번째 문자를 찾기 => 문자의 index
+print(data.find("l"))
+#찾기 => 갯수
+print(data.count("l"))
+print(data.index("e")) #indexOf
+print(data.split(" "))
