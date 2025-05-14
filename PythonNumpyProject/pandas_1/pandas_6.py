a20ac43d2bb419472af2798e385aa889f52ab705
bae <bottleright10@gmail.com>
2025-05-14 09:13:42 +0900

    2025-05-14

---
+++ PythonNumpyProject/pandas_1/pandas_6.py
@@ -0,0 +1,29 @@
+"""
+    Numpy
+    
+    = 수치 계산을 위한 라이브러리
+       ------
+       배열 연산 (백터)
+       선형 대수
+       통계, 난수 생성 => 수학적 기능 제공
+    = 대용량의 데이터 연산을 빠르게 수행할 수 있게
+      최적화된 라이브러리
+    = 다차원 배열 제공 (ndarray)
+        ** 포함하면 안된다
+    = 다른 라이브러리외 연동 : pandas . scikit-learn
+    2. 특수 배열
+       np.zeors / np.ones / np.arange
+       np.linespace(0,10,5)
+    4. 배열 연산
+        +, -, * , /
+        a=np.array([10,20,30])
+        b=np.array([1,2,3])
+        a+b => add(a,b) [11,22,33]
+        a-b => substract(a,b) [9,18,27]
+        a*b => multiple(a,b) [10,40,90]
+        a/b => divide(a,b) [10,10,10]
+    5. 확인
+        행,열=> shape
+        전체 갯수 => size()
+    6. 
+"""
---
+++ PythonNumpyProject/python_numpy/numpy_2.py
@@ -0,0 +1,18 @@
+"""
+    1.list.
+    np.array(list
+   2.배열 형태(행, 열)
+   [1,2,3]
+   [4,5,6]
+    - - -
+    5 7 9  열단위
+
+    ones() => np.ones((2,3))
+    zeros()
+ => np.ser
+    연산 처리 => numpy
+    화면 처리 => maptplotlib, sers
+ """
+import numpa
+
+a=np.arry )]_
